9ff4bd5b3be668650382f6dfa9c608f69f30d57e
Petre Pircalabu <ppircalabu@bitdefender.com>
2019-11-08 15:49:20 +0000

    SQUASHME: af_xen protocol stack
    Signed-off-by: Petre Pircalabu <ppircalabu@bitdefender.com>

---
+++ net/xen/af_xen.c
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/******************************************************************************
+ * af_xen.c
+ *
+ * Implementation of XEN vm_event sockets
+ *
+ * Copyright (c) 2019, Bitdefender S.R.L.
+ */
+
+#define pr_fmt(fmt) "xen:" KBUILD_MODNAME ": " fmt
+
+#include <linux/module.h>
+#include <net/sock.h>
+
+#include <xen/xen.h>
+
+#define VERSION "0.1"
+
+static __init int xen_init(void)
+{
+	if (!xen_domain())
+		return -ENODEV;
+
+	return 0;
+}
+
+static __exit void xen_exit(void)
+{
+}
+
+module_init(xen_init);
+module_exit(xen_exit);
+
+MODULE_AUTHOR("Petre Pircalabu <ppircalabu@bitdefender.com>");
+MODULE_DESCRIPTION("Xen VM event socket ver " VERSION);
+MODULE_VERSION(VERSION);
+MODULE_LICENSE("GPL");
+MODULE_ALIAS_NETPROTO(PF_XEN);
